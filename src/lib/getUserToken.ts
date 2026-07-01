dd323a3894ac9d2720d24ad9d68758a5c60cc489
Abdalrahman Emad <abdoemad1582004@gmail.com>
2026-07-01 10:58:15 +0300

    update in getUserToken

--- src/lib/getUserToken.ts
+++ src/lib/getUserToken.ts
@@ -6,8 +6,9 @@ import { decode } from "next-auth/jwt";
 export async function getDecodedUserToken(){
     const cookie = await cookies()
 
-const token  = cookie.get('next-auth.session-token')?.value ||
-|| cookie.get('__Secure-next-auth.session-token')?.value
+const token  = cookie.get('next-auth.session-token')?.value
+ ||
+ cookie.get('__Secure-next-auth.session-token')?.value
 console.log(token,'tokeeeen 123')
 const decodedToken = await decode({token : token , secret :process.env.NEXTAUTH_SECRET as string})
 console.log(decodedToken?.token,'decodedToken?.token tokeeeen 123')
